ad747debe817efe0d0d6f0570328c03f37e7c687
Songbo <shu2@ed.ac.uk>
2019-07-07 13:11:30 +0200

    Updated

--- temp.js
+++ temp.js
@@ -1,16 +1,6 @@
-//express_demo.js 文件
-var express = require('express');
-var app = express();
-
-app.get('/', function (req, res) {
-    res.send('Hello World');
-});
-
-var server = app.listen(3306, function () {
-
-    var host = server.address().address;
-    var port = server.address().port;
-
-    console.log("应用实例，访问地址为 http://%s:%s", host, port)
-
-});
+var http = require('http');
+var port = process.env.port || 3306;
+http.createServer(function(req, res){
+    res.writeHead(200, {'Content-Type':'text/plain'});
+    res.end('Hello World\n');
+}).listen(port);
